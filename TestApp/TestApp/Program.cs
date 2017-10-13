81edaf5086342e287a47cbe75c62b269d900d27d
wvark <wvark@gmail.com>
2017-10-13 09:09:49 -0500

    Change

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -10,7 +10,8 @@ namespace TestApp
     {
         static void Main(string[] args)
         {
-            // I am changing the code to commit a change to git and make a comment about the checkin.
+            // I am changing the code to commit a change to git and make a comment about the checkin.  
+            // This is more changes
         }
     }
 }
