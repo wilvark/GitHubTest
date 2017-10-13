abe491567792629d629230264c92a33bf5143315
wvark <wvark@gmail.com>
2017-10-13 08:59:35 -0500

    Test change

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // I am changing the code to commit a change to git and make a comment about the checkin.
+        }
+    }
+}
